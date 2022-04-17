f03167d7e0b8a281506a4dca7a41ee40e859da58
siushunking <85872659+siushunking@users.noreply.github.com>
2022-04-18 02:05:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<section>
+  <h3> MY Friends </h3>
+  <ul>
+    <li>
+      <friend-contact /> 
+    </li>
+  </ul>
+ </section>   
+</template>
+
+<script>
+export default{
+  data() {
+    return {
+      friends:[
+        {
+          id: 'manuel',
+          name: 'ivan',
+          phone: '123456789',
+          email: 'manuel@gmail.com'
+        },
+          {
+          id: 'manuel',
+          name: 'ivan',
+          phone: '123456789',
+          email: 'manuel@gmail.com'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2> {{friends.name}} </h2>
+        <button @click="toggleDetails"> show Details </button>
+
+        <ul v-if="detailAreVisible">
+            <li> <strong> Phone: </strong> {{ friends.phone}} </li>
+            <li> <strong> Email: </strong> {{ friends.email}} </li>
+        </ul>    
+    </li>    
+</template>
+
+
+<script>
+export default {
+    data() {
+        return{
+          detailAreVisible: false,
+          friends:  {
+          id: 'manuel',
+          name: 'ivan',
+          phone: '123456789',
+          email: 'manuel@gmail.com'
+        },
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailAreVisible = !this.detailAreVisible;
+        }
+    }
+}
+</script>
